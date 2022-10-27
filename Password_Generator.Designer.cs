baea2503dc5035f168650e84af2f78d0c40bed84
CarbonMaster <uyfU18dGfN6Avfa3c4Ryo93uyfU18dGfN6Av@protonmail.com>
2022-10-27 21:48:19 +0200

    Whole project

---
+++ Password_Generator.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace PasswordGeneratorApp
+{
+    partial class Password_Generator
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Password_Generator));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Password_length = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Generated_password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Roll = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Pass_txt_name = new System.Windows.Forms.TextBox();
+            this.Save_to_txt_button = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(797, 453);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Password_length
+            // 
+            this.Password_length.Location = new System.Drawing.Point(289, 10);
+            this.Password_length.Name = "Password_length";
+            this.Password_length.Size = new System.Drawing.Size(100, 20);
+            this.Password_length.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(49, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Roll the password!";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Generated_password
+            // 
+            this.Generated_password.Location = new System.Drawing.Point(53, 98);
+            this.Generated_password.Multiline = true;
+            this.Generated_password.Name = "Generated_password";
+            this.Generated_password.Size = new System.Drawing.Size(306, 146);
+            this.Generated_password.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(115, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Generated password";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Roll
+            // 
+            this.Roll.Location = new System.Drawing.Point(250, 43);
+            this.Roll.Name = "Roll";
+            this.Roll.Size = new System.Drawing.Size(75, 23);
+            this.Roll.TabIndex = 8;
+            this.Roll.Text = "Roll";
+            this.Roll.UseVisualStyleBackColor = true;
+            this.Roll.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(101, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 24);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Save password to txt file";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Pass_txt_name
+            // 
+            this.Pass_txt_name.Location = new System.Drawing.Point(210, 278);
+            this.Pass_txt_name.Name = "Pass_txt_name";
+            this.Pass_txt_name.Size = new System.Drawing.Size(100, 20);
+            this.Pass_txt_name.TabIndex = 10;
+            // 
+            // Save_to_txt_button
+            // 
+            this.Save_to_txt_button.Location = new System.Drawing.Point(166, 304);
+            this.Save_to_txt_button.Name = "Save_to_txt_button";
+            this.Save_to_txt_button.Size = new System.Drawing.Size(75, 23);
+            this.Save_to_txt_button.TabIndex = 11;
+            this.Save_to_txt_button.Text = "Save";
+            this.Save_to_txt_button.UseVisualStyleBackColor = true;
+            this.Save_to_txt_button.Click += new System.EventHandler(this.Save_to_txt_button_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(102, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Name of text file";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(24, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(243, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "How long the passsword is going to be?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.Save_to_txt_button);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.Pass_txt_name);
+            this.panel1.Controls.Add(this.Password_length);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.Roll);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.Generated_password);
+            this.panel1.Location = new System.Drawing.Point(381, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(404, 347);
+            this.panel1.TabIndex = 13;
+            // 
+            // Password_Generator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Password_Generator";
+            this.Text = "Password generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox Password_length;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Generated_password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button Roll;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Pass_txt_name;
+        private System.Windows.Forms.Button Save_to_txt_button;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Password_Generator.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PasswordGeneratorApp
+{
+    public partial class Password_Generator : Form
+    {
+        public Password_Generator()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Generator generator = new Generator();
+            Generated_password.Text = generator.Pass_roll(Password_length.Text);
+            
+        }
+
+        private void Save_to_txt_button_Click(object sender, EventArgs e)
+        {
+            if(Pass_txt_name.Text == "")
+            {
+
+            }
+            else
+            {
+                saveFile(Generated_password.Text, Pass_txt_name.Text);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        public void saveFile(string input, string title)
+        {
+            string dirParameter = AppDomain.CurrentDomain.BaseDirectory + @"\" + title + ".txt";
+            FileStream fParameter = new FileStream(dirParameter, FileMode.Create, FileAccess.Write);
+            StreamWriter m_WriterParameter = new StreamWriter(fParameter);
+            m_WriterParameter.BaseStream.Seek(0, SeekOrigin.End);
+            m_WriterParameter.Write(input);
+            m_WriterParameter.Flush();
+            m_WriterParameter.Close();
+            System.Windows.Forms.MessageBox.Show("File saved");
+        }
+    }
+
+    public class Functions
+    {
+        public string Generate(int pass_length, string password, string characters, char[] letter_array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < pass_length; i++)
+            {
+                password += letter_array[random.Next(0, letter_array.Length - 1)];
+            }
+            return password;
+        }
+    }
+
+    public class Generator
+    {
+        public string Pass_roll(string pass_length)
+        {
+            string password = "";
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string specChar = "!#$%&'()*+-/:;<=?@[_{|~";
+            string allCharacters = letters + specChar;
+            char[] array_of_characters = allCharacters.ToCharArray();
+            Functions instance = new Functions();
+            int length = 0;
+                    try
+                    {
+                        length = Convert.ToInt32(pass_length);
+
+                    }
+                    catch (System.FormatException)
+                    {
+                    }
+                password = instance.Generate(length, password, allCharacters, array_of_characters);
+            return password;
+        }
+    }
+}
